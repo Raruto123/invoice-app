eea3de8c8766462850b1c7b9219e0d3b124d6688
mohamedsane <sanem9206@gmail.com>
2024-11-28 00:36:35 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+//si je veux faire import { app, BrowserWindow } from "electron" je dois mettre type = module dans index.html et à parti de là ça ressemble à React où je peux importer des bouts de code faire export etc.
+// import { app, BrowserWindow } from "electron";
+const {app, BrowserWindow} = require("electron");
+const { ipcMain } = require("electron/main");
+const path = require("node:path");
+
+const createWindow = () => {
+    const window = new BrowserWindow({
+        width : 800,
+        height : 600,
+        webPreferences : {
+            preload : path.join(__dirname, "preload.js"),
+        }
+    });
+
+    window.loadFile("index.html");
+};
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+});
+
+function realVerification(num) {
+    return num
+}
+
+app.whenReady().then(() => {
+  
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    });
+    ipcMain.handle("verification", () => realVerification("Final Fantasy XV"))
+    createWindow()
+
+})
---
+++ preload.js
@@ -0,0 +1,9 @@
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  verification : () => ipcRenderer.invoke("verification")
+  // we can also expose variables, not just functions
+})
